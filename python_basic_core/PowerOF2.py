68361684d738c764d03936c18a318db142780c0d
manojkr30 <manojramesh3001@gmail.com>
2024-07-24 11:16:50 +0530

    [Manoj KR]Add:Power of 2

---
+++ python_basic_core/PowerOF2.py
@@ -0,0 +1,9 @@
+
+
+N = int(input("Enter the year :"))
+for year in range()
+def findLeapYear(year):
+    if((year % 4 == 0 and year % 100 != 0 ) or year % 400 == 0):
+        print(year,"is a Leap Year")
+    else:
+        print(year,"is Not Leap Year")
